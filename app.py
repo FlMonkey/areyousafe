6d7db17c8fd22c57ef1f3815eb9d9b8cc39f72e8
FlMonkey <75172047+FlMonkey@users.noreply.github.com>
2023-03-24 09:39:49 +0100

    a lot of improvment

--- app.py
+++ app.py
@@ -2,21 +2,26 @@ from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/join')
 def join():
     return render_template('join.html')
 
+
 @app.route('/create')
 def create():
     return render_template('create.html')
 
+
 @app.route('/signin')
 def signin():
     return render_template('signin.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
